a9e709d7d25ed967ee74e0f679a3734b1d7e9255
BIT Student <classroom@bgit.rs>
2017-10-12 17:57:30 +0200

    sdasd

---
+++ conditions/funkcije1.js
@@ -0,0 +1,14 @@
+
+function isString(a) {
+
+    if (typeof (a) === 'string') {
+        console.log("True");
+        return true;}
+    if (typeof (a) === 'number') {
+        console.log("False")
+            return false;
+        }
+    }
+
+
+isString();
